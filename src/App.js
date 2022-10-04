3985ecc25ab6cf19554a15f0ec360d9283ee2613
sydney <becher.sydney@gmail.com>
2022-10-04 11:49:48 +0800

    added lazy loading to load only needed component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import AllQoutes from "./components/pages/AllQoutes";
-import NewQoute from "./components/pages/NewQoute";
-import NotFound from "./components/pages/NotFound";
-import QouteDetail from "./components/pages/QouteDetail";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+// lazy loading for deployment
+const NewQoute = React.lazy(() => import("./components/pages/NewQoute"));
+const QouteDetail = React.lazy(() => import("./components/pages/QouteDetail"));
+const NotFound = React.lazy(() => import("./components/pages/NotFound"));
+const AllQoutes = React.lazy(() => import("./components/pages/AllQoutes"));
 
 function App() {
     return (
         <Layout>
-            <Switch>
-                <Route path="/" exact>
-                    <Redirect to="/qoutes" />
-                </Route>
+            <Suspense
+                fallback={
+                    <div className="centered">
+                        <LoadingSpinner />
+                    </div>
+                }
+            >
+                <Switch>
+                    <Route path="/" exact>
+                        <Redirect to="/qoutes" />
+                    </Route>
 
-                <Route path="/qoutes" exact>
-                    <AllQoutes />
-                </Route>
-                <Route path="/qoutes/:qouteId">
-                    <QouteDetail />
-                </Route>
-                <Route path="/new-qoute">
-                    <NewQoute />
-                </Route>
-                {/* must last, all unknown url will display this */}
-                <Route path="*">
-                    <NotFound />
-                </Route>
-            </Switch>
+                    <Route path="/qoutes" exact>
+                        <AllQoutes />
+                    </Route>
+                    <Route path="/qoutes/:qouteId">
+                        <QouteDetail />
+                    </Route>
+                    <Route path="/new-qoute">
+                        <NewQoute />
+                    </Route>
+                    {/* must last, all unknown url will display this */}
+                    <Route path="*">
+                        <NotFound />
+                    </Route>
+                </Switch>
+            </Suspense>
         </Layout>
     );
 }
